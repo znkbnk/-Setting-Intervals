56e8765cdcbea8e77745146fd829713685d92715
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-05-19 18:45:15 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Clock from './Clock';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Clock App</h1>
+      <Clock />
+    </div>
+  );
+};
+
+export default App;
