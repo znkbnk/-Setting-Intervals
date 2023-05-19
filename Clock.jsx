ed1faae1b0ae8a1742cd369de325c23dc6e7a054
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-05-19 18:36:28 +0100

    Create Clock.jsx

---
+++ Clock.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Current Time:</h1>
+      <p>{time.toLocaleTimeString()}</p>
+    </div>
+  );
+};
+
+export default Clock;
